8339ac0256d770af1758bbe500dc2eac45fbfc8e
ROHIT KUMAR <rohitkumar@ROHITs-iMac.local>
2020-08-14 12:05:32 +0530

    completed with ssl code

--- src/main.ts
+++ src/main.ts
@@ -3,32 +3,55 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import * as config from "config";
 import { Logger } from '@nestjs/common';
+import * as fs from "fs";
+import * as express from "express";
+import * as http from "http";
+import * as https from "https";
+import { ExpressAdapter } from "@nestjs/platform-express";
 
 async function bootstrap() {
   const serverConfig = config.get('server');
-  const app = await NestFactory.create(AppModule);
+  const server = express();
+  const port = process.env.PORT || serverConfig.port;
   const logger = new Logger('MCA Application');
+  let app;
 
-  if (process.env.NODE_ENV === 'development') {
-    app.enableCors();
-  } else {
+  if (process.env.NODE_ENV === 'production') {
+    const httpsOptions = {
+      key: fs.readFileSync('/etc/letsencrypt/live/martinschildrenacademy.com/privkey.pem', 'utf8'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/martinschildrenacademy.com/cert.pem', 'utf8'),
+      ca: fs.readFileSync('/etc/letsencrypt/live/martinschildrenacademy.com/chain.pem', 'utf8')
+    };
+    app = await NestFactory.create(AppModule, new ExpressAdapter(server));
     app.enableCors({
-      origin: serverConfig.origin
+      origin: ['*', serverConfig.origin]
     });
-    logger.log(`Accepting requests from origin ${serverConfig.origin}`);
+    swaggerInitialization(app);
+    logger.log(`Production: Accepting requests from origin ${serverConfig.origin}`);
+    await app.init();
+    https.createServer(httpsOptions, server).listen(443);
+    http.createServer(function(req, res) {
+      res.writeHead(301, {"Location": "https://" + req.headers['host'] + req.url});
+      res.end();
+    }).listen(port);
+    logger.log(`Application is running on port: ${port}`);
+  } else {
+    app = await NestFactory.create(AppModule);
+    app.enableCors();
+    swaggerInitialization(app);
+    logger.log(`Development Environment is Working`);
+    await app.listen(port);
+    logger.log(`Application is running on port: ${port}`);
   }
+}
 
+function swaggerInitialization(app) {
   const options = new DocumentBuilder()
     .setTitle('MCA')
     .setDescription('Martins Children Academy REST API list')
     .setVersion('1.0').addBearerAuth().build();
-
-    
-
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  const port = process.env.PORT || serverConfig.port;
-  await app.listen(port);
-  logger.log(`Application is running on port: ${port}`);
 }
+
 bootstrap();
